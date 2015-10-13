b84779a4bc5ee3b172922f12283a7882aae7229a
ggustilo <beastifer@gmail.com>
2015-10-12 20:53:10 -0500

    Update GPS solution

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,36 +1,50 @@
 # Your Names
-# 1)
-# 2)
+# 1) Paul Gaston Gouron
+# 2) Gabrielle Gustilo
 
-# We spent [#] hours on this challenge.
+# We spent 2 hours on this challenge.
+
+# Change variable names
+# Refactor error counter
+# Add restocking feature
+# Rewrite messages for clearer instructions
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, order_quantity)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+  # Data
+  pantry = {"cookie" => 1, "cake" =>  5, "pie" => 7}
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
-  end
+  if pantry.has_key?(item_to_make)
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+          stock = (pantry[item_to_make] - order_quantity)
 
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
+          p "We have to make #{order_quantity} #{item_to_make}. By processing this order, the stock of #{item_to_make} will be #{stock} #{item_to_make}." 
 
-  case serving_size_mod
-  when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
-  else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
-  end
+
+          if stock > 0
+                p "We have enough #{item_to_make} to proceed with the order!"
+          elsif stock == 0
+                p "We just have enough stock. The next order will probably put us out of stock. We should make more #{item_to_make}."
+          else 
+            p "We don't have enough stock...to be able to proceed with the order.  We should make #{stock.abs} #{item_to_make}."
+          end 
+          
+          p "We now have:" 
+          
+          pantry.each do |key,value|
+          pantry[item_to_make] = stock
+         
+          p "#{key.capitalize} : #{value}"
+          end
+    
+    else
+    raise ArgumentError.new("#{item_to_make} is not on our menu.")
+    end    
 end
 
+p serving_size_calc("pie", 1)
+p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
 p serving_size_calc("cake", 5)
@@ -39,5 +53,32 @@ p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
-#  Reflection
+# Reflection:
+
+# What did you learn about making code readable by working on this challenge?
+# Did you learn any new methods? What did you learn about them?
+# What did you learn about accessing data in hashes? 
+# What concepts were solidified when working through this challenge?
+
+# Well, I learned that variable names are really really important.  If they are unclear or misleading, it's hard to understand what the code does.  Also, you should always access hash values through the keys and not use arbitrary arrays for no reason.  Also, comments would be nice.
+
+# This was the first time that I saw the values_at method.  In another situation, I'm sure it would be super useful to have an array of hash values.  But in this case, it was really overkill.
+
+# The major concepts that were solidified for me were a) name your variables descriptively; and b) don't unnecessarily complicate your code - be clear to your purpose.  O, and COMMENT YOUR CODE.
+
+# :)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
