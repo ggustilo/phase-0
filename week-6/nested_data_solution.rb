72be78a6408cdac81af8fafce179223333594ea0
ggustilo <beastifer@gmail.com>
2015-10-16 16:18:19 -0500

    Edit stuff

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -67,3 +67,19 @@ startup_names.map! do |element|
 end
 
 p startup_names
+
+# Reflection:
+
+# What are some general rules you can apply to nested arrays?
+# What are some ways you can iterate over nested arrays?
+# Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+
+# Well, I would start by making sure that you are checking the number of levels in the array.  If there are 5 levels, you are going to need 5 indices, or 5 loops.  Also, look carefully to see what's inside what and where.  Make notes if you need to.  Go step by step.
+
+# You can iterate over nested arrays using nested loops, or you can flatten them and then iterate.  Or you, could go through element by element I suppose, called different indices.
+
+# I used map! in both solutions, which seems to be the method I gravitate towards.  I tend to like destructive methods unless I am told to do otherwise, or I see a specific reason why I should preserve the data as is.  It's just easier to see what's going on, and I like the linear progression of changes.  I could have also used map or each and just made a new array.  Or I could have used collect or collect!.
+
+
+
+
