9dc4d539440a5fc6575ef03e509d332a5a1e310b
ggustilo <beastifer@gmail.com>
2015-10-21 17:23:16 -0500

    Initialize group project, and accomplish step 1

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,14 @@
+// Release 1: Tests to User Stories
+// Look at the tests.js file in this directory.
+
+// Based on the tests, write user stories that describe what the code needs to do. User stories take the following format:
+
+// As a user, I want to..
+
+// USER STORIES:
+
+// As a user, I want to be able to find the sum of any array comprised of integers by calling a function called 'sum'.
+
+// As a user, I want to be able to find the mean of any array comprised of integers by calling a function called 'mean'.
+
+// As a user, I want to be able to find the median of any array comprised of integers by calling a function called 'median'.
